502f2249bc93abae1e93688c57a1b416c88aba5e
ivan.ratalo <ivan.ratalo@educa.jcyl.es>
2025-11-12 11:02:17 +0100

    Primera version del proyecto:

---
+++ app/src/main/java/es/ies/claudiomoyano/dam2/pmdm/planificador_de_viajes_rayon_alonso_ivan/ActividadDetalleViaje.java
@@ -0,0 +1,20 @@
+package es.ies.claudiomoyano.dam2.pmdm.planificador_de_viajes_rayon_alonso_ivan;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ActividadDetalleViaje extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_actividad_detalle_viaje);
+
+    }
+}
